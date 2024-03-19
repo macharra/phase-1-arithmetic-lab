f7a6069a9e30e09579d210fb56f8de67fb9e4d3f
rogers.machara <rogersmachara13@gmail.com>
2024-03-19 12:50:49 +0300

    shows arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+// let Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10);
+
+const num3 = 20;
+const num4 = 8;
+const mod = num3 % num4;
+
+const numbers = [3, 20, 16, 8];
+const max = Math.max(...numbers);
